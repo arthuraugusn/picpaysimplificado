080981f407b6537a1b79f50a208d2a0e37dda61d
Arthur Augusto <arthur.augusto.sn@gmail.com>
2024-01-29 17:35:47 -0300

    create Transaction entity

---
+++ picpaysimplificado/src/main/java/com/example/picpaysimplificado/domain/transaction/Transaction.java
@@ -0,0 +1,33 @@
+package com.example.picpaysimplificado.domain.transaction;
+
+import com.example.picpaysimplificado.domain.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name = "transactions")
+@Table(name = "tbl_transaction")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private BigDecimal amount;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+
+    private LocalDateTime timestamp;
+
+}
